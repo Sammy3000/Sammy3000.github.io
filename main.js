ee1a56c74fa0b289a109dca75bd20a956d0eaa8c
Sammy Towett <towettsammy30@gmail.com>
2022-12-19 15:08:54 +0300

    add close icon in images folder. create class of active and activate it using JS when the menu icon is clicked

---
+++ main.js
@@ -0,0 +1,5 @@
+let hambugerMenu = document.querySelector(".nav-img");
+let desktopNavigation = document.querySelector(".desktop-nav");
+hambugerMenu.addEventListener("click", () => {
+  hambugerMenu.classList.toggle("active");
+});
