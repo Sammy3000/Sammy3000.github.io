2f2413c0585ff4e4769dc45c950f29ead1d6a9dd
Sammy Towett <towettsammy30@gmail.com>
2022-12-24 19:47:36 +0300

    fix bug

--- Menuicon.js
+++ Menuicon.js
@@ -1,7 +1,7 @@
 const menuIcon = document.querySelector(".nav-icons");
-const mainNavigation = document.querySelector("main-nav");
+const mainNavigation = document.querySelector(".main-nav");
 
-menuIcon.addEventListener("click", (n) => {
+menuIcon.addEventListener("click", () => {
   mainNavigation.classList.toggle("nav-open");
   document.body.classList.toggle("hidden");
 });
